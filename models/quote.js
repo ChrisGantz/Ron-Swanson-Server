cafb5fdbe9ec71af303c0857628217a948faf1fc
Chris Gantz <chris.gonzalez836@gmail.com>
2019-03-20 23:24:57 -0700

    added quote schema

---
+++ models/quote.js
@@ -0,0 +1,21 @@
+/* eslint-disable strict */
+const mongoose = require('mongoose');
+
+const quoteSchema = new mongoose.Schema({
+  quote: [{ type: String, required: true }],
+  rating: { type: Number },
+  localSession: { type: String },
+  cookieSession: { type: String }
+});
+
+quoteSchema.set('timestamps', true);
+
+quoteSchema.set('toObject', {
+  virtuals: true,
+  versionKey: false,
+  transform: (doc, ret) => {
+    delete ret._id;
+  }
+});
+
+module.exports = mongoose.model('Quote', quoteSchema);

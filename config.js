61ce1abd53c1adbf7bde02473aaa4b7e9b1cd5f5
Chris Gantz <chris.gonzalez836@gmail.com>
2019-03-20 21:57:40 -0700

    server setup complete

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  PORT: process.env.PORT || 8080,
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || "http://localhost:3000",
+  MONGODB_URI: process.env.MONGODB_URI || "mongodb://localhost/ron-swanson-quotes",
+  TEST_MONGODB_URI:
+    process.env.TEST_MONGODB_URI || "mongodb://localhost/ron-swanson-quotes-test",
+};
---
+++ server.js
@@ -0,0 +1,62 @@
+/* eslint-disable strict */
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const morgan = require('morgan')
+
+const { PORT, MONGODB_URI, CLIENT_ORIGIN } = require('./config');
+
+
+const app = express();
+
+
+app.use(
+  morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
+    skip: (req, res) => process.env.NODE_ENV === 'test'
+  })
+);
+
+app.use(
+  cors({
+    origin: CLIENT_ORIGIN
+  })
+);
+
+// mount routes
+
+// Custom Error Handler
+app.use((err, req, res, next) => {
+  if (err.status) {
+    const errBody = Object.assign({}, err, { message: err.message });
+    res.status(err.status).json(errBody);
+  } else {
+    console.error(err);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+});
+
+if (process.env.NODE_ENV !== 'test') {
+  mongoose
+    .connect(MONGODB_URI)
+    .then(instance => {
+      const conn = instance.connections[0];
+      console.info(
+        `Connected to: mongodb://${conn.host}:${conn.port}/${conn.name}`
+      );
+    })
+    .catch(err => {
+      console.error(`ERROR: ${err.message}`);
+      console.error('\n === Did you remember to start `mongod`? === \n');
+      console.error(err);
+    });
+  app
+    .listen(PORT, function () {
+      console.info(`Server listening on ${this.address().port}`);
+    })
+    .on('error', err => {
+      console.error(err);
+    });
+}
+
+module.exports = app;
